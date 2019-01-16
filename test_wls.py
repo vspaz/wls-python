847e64222645effe81c47bcc8f7e52f8c064930f
vspaz <vladimir.edelman@gmail.com>
2019-01-16 22:24:21 +0100

    add wls model

---
+++ test_wls.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+from array import array
+import unittest
+
+from wls import wls
+
+
+class TestWLS(unittest.TestCase):
+    def _assert_regression_model(self, y, x, w, expected):
+        expected_intercept, expected_slope = expected
+        got_intercept, got_slope = wls.fit_linear_regression(y, x, w)
+        self.assertAlmostEqual(expected_intercept, got_intercept, places=6)
+        self.assertAlmostEqual(expected_slope, got_slope, places=6)
+
+    def test_wls_model_stable_weight(self):
+        y = [1, 3, 4, 5, 2, 3, 4]
+        x = [1, 2, 3, 4, 5, 6, 7]
+        w = 1
+
+        self._assert_regression_model(y, x, w, expected=(2.14285714, 0.25))
+
+    def test_wls_model_weight(self):
+        y = [1, 3, 4, 5, 2, 3, 4]
+        x = [1, 2, 3, 4, 5, 6, 7]
+        w = 0.9
+
+        self._assert_regression_model(y, x, w, expected=(2.14285714, 0.25))
+
+    def test_single_point_disallowed(self):
+        # this behavior differs from statsmodels.WLS
+        x = [10]
+        y = [1]
+        with self.assertRaises(AssertionError):
+            wls.fit_linear_regression(y, x, 1)
+
+    def test_horizontal_line(self):
+        x = [0, 1]
+        y = [10, 10]
+        w = 1
+
+        self._assert_regression_model(y, x, w, expected=(10, 0))
+
+    def test_vertical_line(self):
+        # this behavior differs from statsmodels.WLS
+        x = [1, 1]
+        y = [0, 1]
+        w = 1
+        intercept, slope = wls.fit_linear_regression(y, x, w)
+
+        self.assertIsNone(intercept)
+        self.assertIsNone(slope)
+
+    def test_run_uphill(self):
+        x = [0, 1]
+        y = [0, 1]
+        w = 1
+        self._assert_regression_model(y, x, w, expected=(0, 1))
+
+    def test_run_downhill(self):
+        x = [1, 0]
+        y = [0, 1]
+        w = 1
+        self._assert_regression_model(y, x, w, expected=(1., -1.))
+
+    def test_wrong_parameters_length(self):
+        x = [0, 1, 2, 3]
+        y = [1, 2, 0, 1, 1]
+        w = 1
+        with self.assertRaises(AssertionError):
+            wls.fit_linear_regression(y, x, w)
+
+    def test_wrong_weight_type(self):
+        x = [0, 1, 2, 3, 1]
+        y = [1, 2, 0, 1, 1]
+        w = None
+        with self.assertRaises(AssertionError):
+            wls.fit_linear_regression(y, x, w)
+
+    def test_not_iterable_type_should_fail(self):
+        x = 10
+        y = [1, 2, 0, 1, 1]
+        w = 1
+        with self.assertRaises(AssertionError):
+            wls.fit_linear_regression(y, x, w)
+
+    def test_iterable_parameters_allowed(self):
+        x = (0, 1, 2, 3, 4)
+        y = array('i', [1, 2, 0, 1, 1])
+        w = 1
+        self._assert_regression_model(y, x, w, expected=(1.2, -0.1))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ wls/wls.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+########################################################################
+#
+# wls - Calculate slope & intercept using weighted least squares method.
+# inspired by statsmodels.regression.linear_model.WLS
+# https://www.statsmodels.org/dev/generated/statsmodels.regression.linear_model.WLS.html
+#
+########################################################################
+
+
+from collections import Iterable
+from numbers import Number
+
+
+def fit_linear_regression(y, x, w=1):
+    """fits WLS linear regression"""
+    assert isinstance(y, Iterable) and isinstance(x, Iterable)
+    assert isinstance(w, Iterable) or isinstance(w, Number)
+    elements_count = len(x)
+    assert elements_count >= 2
+
+    if isinstance(w, Number):
+        w = [w] * elements_count
+
+    assert len(w) == len(y) == elements_count
+
+    sum_w = 0
+    sum_of_w_by_x_squared = 0
+    sum_of_x_by_y_by_w = 0
+    sum_of_x_by_w = 0
+    sum_of_y_by_w = 0
+
+    for x_i, y_i, w_i in zip(x, y, w):
+        sum_w += w_i
+        x_i_by_w_i = x_i * w_i
+        sum_of_x_by_w += x_i_by_w_i
+        sum_of_x_by_y_by_w += x_i_by_w_i * y_i
+        sum_of_y_by_w += w_i * y_i
+        sum_of_w_by_x_squared += x_i_by_w_i * x_i
+
+    dividend = sum_w * sum_of_x_by_y_by_w - sum_of_x_by_w * sum_of_y_by_w
+    divisor = sum_w * sum_of_w_by_x_squared - sum_of_x_by_w ** 2
+    if not divisor:
+        return None, None
+    slope = dividend / divisor
+    intercept = (sum_of_y_by_w - slope * sum_of_x_by_w) / sum_w
+
+    return intercept, slope
