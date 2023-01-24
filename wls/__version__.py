7f9ff76e1982b36ed2e3e42ccc3aa7b25db8f6a0
vspaz <vladimir.edelman@gmail.com>
2023-01-25 00:53:13 +0100

    prepare for packaging

---
+++ wls/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "wls"
+__description__ = "weighted linear regression w/o any dependencies e.g. pandas, numpy, etc"
+__url__ = "https://github.com/vspaz/wls-python"
+__email__ = "wlodzimierz.trzmielinsky@gmail.com"
+__author__ = "vspaz"
+__maintainer__ = "vspaz"
+__version__ = "v0.1.1"
